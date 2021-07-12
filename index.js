37e97cc2978576c2c2de95b24cc9c80a3a842f32
TimVerkleij <56248095+TimVerkleij@users.noreply.github.com>
2021-07-12 13:33:17 +0200

    Add API files

---
+++ index.js
@@ -0,0 +1,19 @@
+const Express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = Express()
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(bodyParser())
+app.use(require('./routes/api'))
+
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
+
+console.log("listening on port: " + process.env.PORT)
+app.listen(process.env.PORT);
